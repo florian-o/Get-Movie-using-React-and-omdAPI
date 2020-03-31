17b65ffcc9d18b5ebf3d6e5bad825f0118b01a5d
florian <florian.odouard.wave@gmail.com>
2020-03-31 11:34:32 +0200

    add func search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 // Import Components
 import Navbar from './components/layout/Navbar';
 import SearchBar from './components/SearchBar';
 
 
+
+
+
 function App() {
+
+  const [state, setState] = useState({
+    search:"",
+    results:[],
+    selected:{}
+  });
+
+  const search = evt => {
+    if (evt.key === 'Enter') {
+      console.log('yes');      
+    };
+  };
+
+  const handleInput = evt => {
+
+    let search = evt.target.value;
+    console.log(search);
+
+  };
+
   return (
 
     <div>
       <Navbar />
       <div className="container">        
-        <SearchBar />      
+        <SearchBar handleInput={handleInput} search={search} />      
       </div>
     </div>
   );
