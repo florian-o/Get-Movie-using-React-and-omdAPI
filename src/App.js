f2ba8ecc0931a70745b6c75c47cf6c877d3ae1fc
florian <florian.odouard.wave@gmail.com>
2020-03-31 12:01:30 +0200

    adding axios and connect to api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react';
+import axios from "axios";
 
 // Import Components
 import Navbar from './components/layout/Navbar';
@@ -10,23 +11,43 @@ import SearchBar from './components/SearchBar';
 
 function App() {
 
+
+
+
   const [state, setState] = useState({
     search:"",
     results:[],
-    selected:{}
   });
 
+  
+  const apiKey = "http://www.omdbapi.com/?apikey=4e8007ba";
+
+
   const search = evt => {
+
     if (evt.key === 'Enter') {
-      console.log('yes');      
+      
+      // Send the request
+      axios(apiKey + "&s=" + state.search)
+        .then(({data})=>{
+          let results = data;
+          setState({
+            results:results
+          });
+          console.log(results);          
+        });  
     };
+     
   };
 
   const handleInput = evt => {
 
     let search = evt.target.value;
-    console.log(search);
-
+    
+    setState({
+      search:search
+    });
+   
   };
 
   return (
